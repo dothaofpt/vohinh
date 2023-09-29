9df444abc78fa80bcebb35a45582ef02dc7e8bc1
Do Thao <DoThao@gmail.com>
2023-09-29 18:58:11 +0700

    baitapjs
    js

---
+++ b9/main.js
@@ -0,0 +1,11 @@
+/** kha nang hiien thi javascript
+ * viet vao 1 phan tu html-innerHTML
+ * viet vao dau ra html-documment.write().
+ * viet vao hop canh bao- window.alert().
+ * viet vao bang dieu khien trinh duyet su dung console.log().
+ */
+/** su dung html ben trong
+ * truy cap vao phan tu html-document.getElementId(id)
+ * atrribute id:xac dinh phan tu html
+ * atrribute innerHTML: xac dinh noi dung html
+ */
